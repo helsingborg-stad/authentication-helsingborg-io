df79cf66ad88fcafbe1a4141dc58df24a5c4b0ef
Eldin Behlic <eldin.behlic@hiq.se>
2018-11-20 16:35:42 +0100

    - Added initial bankid logic to the project.

--- tier3-server/navet-endpoint/components/index.js
+++ tier3-server/navet-endpoint/components/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/getPerson', require('./person/personApi'));
+router.use('/bankid', require('./bankid/bankIdApi'));
 
 module.exports = router;
